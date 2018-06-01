80e26625050455dac9b0b2465ca19c191bf8f4a9
Nicholas Lambourne <ndl93@live.com>
2018-06-01 12:43:51 -0700

    finished routing

--- ps-react-flux-apps/src/components/about/aboutPage.js
+++ ps-react-flux-apps/src/components/about/aboutPage.js
@@ -3,6 +3,23 @@
 var React = require('react');
 
 var About = React.createClass({
+    statics: {
+        willTransitionTo: function(transition, params, query, callback) {
+            if (!confirm("Are you sure you want to proceed?")) {
+                transition.abort();
+            } else {
+                callback();
+            }
+        },
+        willTransitionFrom: function(transition, component) {
+            if (!confirm("Are you sure you want to proceed?")) {
+                transition.abort();
+            }
+        }
+    },
+
+
+
     render: function () {
         return (
             <div>

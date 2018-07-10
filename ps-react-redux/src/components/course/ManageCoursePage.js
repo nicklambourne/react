9fafccaf665e8cabea69f18b3c50dd0621be729e
Nicholas Lambourne <ndl93@live.com>
2018-07-10 22:27:38 +1000

    form prefill

--- ps-react-redux/src/components/course/ManageCoursePage.js
+++ ps-react-redux/src/components/course/ManageCoursePage.js
@@ -18,6 +18,12 @@ class ManageCoursePage extends React.Component {
     this.saveCourse = this.saveCourse.bind(this);
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (this.props.course.id !== nextProps.course.id) {
+      this.setState({course: Object.assign({}, nextProps.course)});
+    }
+  }
+
   updateCourseState(event) {
     const field = event.target.name;
     let course = Object.assign({}, this.state.course);
@@ -57,14 +63,28 @@ ManageCoursePage.contextTypes = {
   router: PropTypes.object
 };
 
+function getCourseById (courses, id) {
+  const course = courses.filter(course => course.id = id);
+  if (course.length) return course[0];
+  return null;
+}
+
 function mapStateToProps(state, ownProps) {
+  const courseId = ownProps.params.id;
+
   let course = {id: '', watchHref: '', authorId: '', length: '', category: ''};
+
+  if (courseId && state.courses.length > 0) {
+    course = getCourseById(state.courses, courseId);
+  }
+
   const authorsFormattedForDropdown = state.authors.map(author => {
     return {
       value: author.id,
       text: author.firstName + ' ' + author.lastName
     };
   });
+
   return {
     course: course,
     authors: authorsFormattedForDropdown
--- ps-react-redux/src/routes.js
+++ ps-react-redux/src/routes.js
@@ -13,6 +13,6 @@ export default (
     <Route path="about" component={AboutPage}/>
     <Route path="courses" component={CoursesPage}/>
     <Route path="course" component={ManageCoursePage}/>
-    <Route path="course/:id?" component={ManageCoursePage}/>
+    <Route path="course/:id" component={ManageCoursePage}/>
   </Route>
 );
